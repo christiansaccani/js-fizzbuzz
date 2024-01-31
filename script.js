f0981def319a24238e7f7f000928505ececc8ccc
christiansaccani <christian.saccani99@gmail.com>
2024-01-31 16:58:26 +0100

    Exercise Done! (Bonus Done!)

--- script.js
+++ script.js
@@ -8,11 +8,22 @@ for (let i=1; i<=100; i++) {
     grillElement.append(newElement);
 
     if (i % 3 == 0 && i % 5 != 0) {
+
         newElement.innerHTML = "fizz";
+        newElement.style.backgroundColor = "#dee5e3";
+        newElement.style.color = "black";
+
     } else if (i % 3 != 0 && i % 5 == 0) {
+
         newElement.innerHTML = "buzz";
+        newElement.style.backgroundColor = "#dad0af";
+        newElement.style.color = "black";
+
     } else if (i % 3 == 0 && i % 5 == 0) {
+
         newElement.innerHTML = "fizzbuzz";
+        newElement.style.backgroundColor = "#c8a566";
+
     } else {
         newElement.innerHTML = i;
     }
